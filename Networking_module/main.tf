317ab9a908b540ebb7ad1ec53f081c04389f42de
Nikita joshi <97650667+nikita647@users.noreply.github.com>
2025-04-02 10:41:29 +0530

    Update main.tf

--- Networking_module/main.tf
+++ Networking_module/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.80.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 resource "aws_vpc" "vpc" {
